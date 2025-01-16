e20314242c25a84700fe363f0f602bb7677e7ba2
taekyu kim <william1204@naver.com>
2025-01-16 16:32:31 +0900

    first

---
+++ src/main/java/com/example/spring_yt/SpringYtApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_yt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringYtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringYtApplication.class, args);
+	}
+
+}
